57eab2ae8bac45e20f096ece773190d3e981b4af
n1sopm <nimomnazar08@gmail.com>
2025-04-18 16:17:04 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+let display = document.getElementById('display');
+function appendNumber(number) {
+display.value += number;
+}
+
+function appendOperator(operator) {
+display.value += ' ' + operator + ' ';
+}
+
+function clearDisplay() {
+display.value = '';
+}
+
+function calculate() {
+try {
+display.value = eval(display.value);
+} catch (error) {
+display.value = 'Ошибка';
+}
+}
